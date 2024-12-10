e30aaae31db6f938aac53ceed51fbb74060bbe89
Ayyappa <pinnepalliayyappa@gmail.com>
2024-12-10 16:28:49 +0530

    Call Details Cloud Config Service

---
+++ telecom-boot_cloudconfig/calldetails/src/test/java/com/application/calldetails/CalldetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.calldetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalldetailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
