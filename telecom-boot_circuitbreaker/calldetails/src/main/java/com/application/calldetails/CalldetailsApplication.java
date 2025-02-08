593b49777a4098511e3d08db52f1f92f04fb8a3b
Ayyappa <pinnepalliayyappa@gmail.com>
2025-02-08 19:21:48 +0530

    Call Details Citrcuit breaker service

---
+++ telecom-boot_circuitbreaker/calldetails/src/main/java/com/application/calldetails/CalldetailsApplication.java
@@ -0,0 +1,13 @@
+package com.application.calldetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalldetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalldetailsApplication.class, args);
+    }
+
+}
