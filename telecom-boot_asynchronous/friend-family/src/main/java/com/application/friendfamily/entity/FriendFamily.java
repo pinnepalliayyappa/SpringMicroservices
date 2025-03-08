4694d0953d317488662fec844fa7643f4f73895b
Ayyappa <pinnepalliayyappa@gmail.com>
2025-03-08 10:27:58 +0530

    api gateway servicde

---
+++ telecom-boot_asynchronous/friend-family/src/main/java/com/application/friendfamily/entity/FriendFamily.java
@@ -0,0 +1,48 @@
+package com.application.friendfamily.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "friendfamily")
+public class FriendFamily {
+
+	@Id
+	@GeneratedValue
+	int id;
+
+	@Column(name = "phone_no")
+	long phoneNo;
+
+	@Column(name = "friend_and_family")
+	long friendAndFamily;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public long getFriendAndFamily() {
+		return friendAndFamily;
+	}
+
+	public void setFriendAndFamily(long friendAndFamily) {
+		this.friendAndFamily = friendAndFamily;
+	}
+
+	public FriendFamily() {
+		super();
+	}
+
+}
